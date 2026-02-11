f1ffb73ea9612ca20670c981572e3ceda4b6dd19
yamaryosan <canda3267@gmail.com>
2026-02-11 22:45:10 +0900

    docs: コメント加筆

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -3,3 +3,9 @@ import { handlers } from '@/app/auth';
 // このルートハンドラは、クライアントサイドでAPIを叩くときにパスを公開するためのものであり
 // 今の実装（サーバサイドでログイン・ログアウトを行う）では不要。そのうち使う
 export const { GET, POST } = handlers;
+
+// こいつらのこと
+// /api/auth/session
+// /api/auth/signin
+// /api/auth/signout
+// /api/auth/callback/credentials

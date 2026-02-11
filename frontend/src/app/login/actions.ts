f5d3b21175a1837f7885c9c1cee2c22ef4f9c18f
yamaryosan <canda3267@gmail.com>
2026-02-11 20:20:06 +0900

    fix: 不要な記述を削除

--- frontend/src/app/login/actions.ts
+++ frontend/src/app/login/actions.ts
@@ -3,12 +3,6 @@
 import { signIn } from '@/app/auth';
 
 export async function signin(formData: FormData) {
-	const data = new URLSearchParams();
-
-	for (const [key, value] of formData.entries()) {
-		data.append(key, value.toString());
-	}
-
 	try {
 		await signIn('credentials', {
 			email: formData.get('email'),
@@ -19,13 +13,11 @@ export async function signin(formData: FormData) {
 		if (error?.type === 'CredentialsSignin') {
 			return { error: 'メールまたはパスワードが正しくありません' };
 		}
-
-		console.log(error);
-
 		if (error instanceof Error && error.message === 'NEXT_REDIRECT') {
 			throw error;
 		}
 
+		console.error(error);
 		throw error;
 	}
 }
